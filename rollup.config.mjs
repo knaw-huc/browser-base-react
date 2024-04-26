fbe282d74fd1d73f07c1048b84a5a5d49c682e1b
kerim1 <kerim.meijer@di.huc.knaw.nl>
2024-04-26 13:49:53 +0200

    Upgrade packages

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,11 +12,11 @@ export default [{
     output: [{
         file: packageJson.main,
         format: 'cjs',
-        //sourcemap: true,
+        sourcemap: true,
     }, {
         file: packageJson.module,
         format: 'esm',
-        //sourcemap: true,
+        sourcemap: true,
     }],
     plugins: [
         peerDepsExternal(),
